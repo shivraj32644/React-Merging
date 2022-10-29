55ebb22cf548fb8be093170e058f4cfc6a8a1317
shivraj32644 <shivraj32644@gmail.com>
2022-10-29 20:08:01 +0530

    Homepage Code

---
+++ src/Components/HomepageCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const HomepageCard = () => {
+  return (
+      <div style={{boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",padding:"20px", borderRadius:"10px", textAlign:"center"}}>
+          <img height={"100px"} src="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?cs=srgb&dl=pexels-math-90946.jpg&fm=jpg" alt="" />
+          <h2>Home Page Product</h2>
+          <h2>₹ 80,000</h2>
+      </div>
+  )
+}
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { HomepageCard } from '../Components/HomepageCard'
+
+export const Home = () => {
+  return (
+      <div style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "80%",
+          margin: "auto",
+          gap: "20px",
+          flexWrap: "wrap",
+          marginTop:"40px"
+      }} >
+        <HomepageCard />
+        <HomepageCard />
+        <HomepageCard />
+        <HomepageCard />
+        <HomepageCard />
+        <HomepageCard />
+        <HomepageCard />
+        <HomepageCard />
+        <HomepageCard />
+        <HomepageCard />
+        <HomepageCard />
+        <HomepageCard />
+        <HomepageCard />
+        <HomepageCard />
+        <HomepageCard />
+        <HomepageCard />
+        <HomepageCard />
+        <HomepageCard />
+        <HomepageCard />
+        <HomepageCard />
+        <HomepageCard />
+        <HomepageCard />
+      </div>
+  )
+}
